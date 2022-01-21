93d019c063eeabd030c49baeb210ac55445f4f97
junam2 <speedlin4584@gmail.com>
2022-01-21 23:12:59 +0900

    feat: 메인페이지 글 리스팅 및 페이지네이션 구현

--- src/main/java/com/cos/blog/controller/BoardController.java
+++ src/main/java/com/cos/blog/controller/BoardController.java
@@ -1,14 +1,23 @@
 package com.cos.blog.controller;
 
+import com.cos.blog.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BoardController {
 
+    @Autowired
+    private BoardService boardService;
+
     @GetMapping({"","/"})
-    public String index() {
-        // /WEB-INF/views/index.jsp
+    public String index(Model model, @PageableDefault(size = 3, sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
+        model.addAttribute("boards", boardService.getList(pageable));
         return "index";
     }
 
--- src/main/java/com/cos/blog/service/BoardService.java
+++ src/main/java/com/cos/blog/service/BoardService.java
@@ -4,6 +4,8 @@ import com.cos.blog.model.Board;
 import com.cos.blog.model.User;
 import com.cos.blog.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,24 +14,18 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class BoardService {
 
-    /*
-    서비스를 사용하는 이유.
-
-    1. 트랜잭션 관리
-
-    2. 서비스 의미
-    - repository 의 CRUD 의 여러개 트랜잭션을 하나의 트랜잭션으로 만들어서 서비스로 사용 가능하다.
-    */
-
     @Autowired
     private BoardRepository boardRepository;
-
 
     @Transactional
     public void write(Board board, User user) {
         board.setCount(0);
         board.setUser(user);
         boardRepository.save(board);
+    }
+
+    public Page<Board> getList(Pageable pageable ) {
+        return boardRepository.findAll(pageable);
     }
 
 }
