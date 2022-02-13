111fcab9a72018ef10c9ea87d711fabbb05ebd86
junam2 <speedlin4584@gmail.com>
2022-02-13 21:17:52 +0900

    feat: 회원 정보 수정 시 세션 수정 기능 구현

--- src/main/java/com/cos/blog/controller/api/UserApiController.java
+++ src/main/java/com/cos/blog/controller/api/UserApiController.java
@@ -5,6 +5,10 @@ import com.cos.blog.model.User;
 import com.cos.blog.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +20,9 @@ public class UserApiController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
     @PostMapping("/auth/joinProc")
     public ResponseDto<Integer> save(@RequestBody User user) {
         int result = userService.Join(user);
@@ -25,6 +32,15 @@ public class UserApiController {
     @PutMapping("/user")
     public ResponseDto<Integer> update(@RequestBody User user) {
         userService.editUser(user);
+
+        // 트랜잭션이 종료되기 때문에 DB 값은 변경
+        // 세션 값은 변경되지 않은 상태 -> 세션 값 변경 필요
+
+        // 세션 등록 (데이터 변경이 일어난 이후에 세션을 등록해야 변경한 값으로 세션을 등록할 수 있다.)
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword())
+        );
+        SecurityContextHolder.getContext().setAuthentication(authentication);
 
         return new ResponseDto<Integer>(HttpStatus.OK.value(),1);
     }
